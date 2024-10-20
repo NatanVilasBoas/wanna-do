fb02ca65a58bfe03449ed70c289f7a1e676b144a
Natan Vilas Boas <natanvilasboas@MacBook-Pro-de-Natan.local>
2024-10-20 16:15:27 -0300

    config prettier, add custom backgroundColor for CustomStatusBar

--- src/components/organisms/CustomStatusBar/index.tsx
+++ src/components/organisms/CustomStatusBar/index.tsx
@@ -3,14 +3,15 @@ import { View } from "react-native"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 
 import { StatusBar } from "expo-status-bar"
+
 import theme from "../../../styles/theme"
 
-export default function CustomStatusBar() {
+export default function CustomStatusBar({ backgroundColor }: { backgroundColor?: string }) {
   const insets = useSafeAreaInsets()
 
   return (
-    <View style={{ height: insets.top, backgroundColor: theme.colors.secondaryMain }}>
-      <StatusBar translucent animated backgroundColor={theme.colors.secondaryMain} />
+    <View style={{ height: insets.top, backgroundColor: backgroundColor ?? theme.colors.secondaryMain }}>
+      <StatusBar translucent animated backgroundColor={backgroundColor ?? theme.colors.secondaryMain} />
     </View>
   )
 }
--- src/pages/AddTask/index.tsx
+++ src/pages/AddTask/index.tsx
@@ -1,13 +1,18 @@
 import Title from "../../components/atoms/Title"
 import Input from "../../components/molecules/Input"
+import CustomStatusBar from "../../components/organisms/CustomStatusBar"
+import theme from "../../styles/theme"
 import { Container } from "./styles"
 
 export default function AddTask() {
   return (
-    <Container>
-      <Title>Nova tarefa</Title>
-      <Input label="Título da tarefa" />
-      <Input label="Descrição" />
-    </Container>
+    <>
+      <CustomStatusBar backgroundColor={theme.colors.secondaryLight} />
+      <Container>
+        <Title>Nova tarefa</Title>
+        <Input label="Título da tarefa" />
+        <Input label="Descrição" />
+      </Container>
+    </>
   )
 }
